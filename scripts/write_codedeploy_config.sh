a3b9992c3f63a912565df7c551bdb9d4f301596f
cb13579 <christian.breitenstrom@berlin.de>
2019-01-12 08:50:27 +0100

    directory changed

--- scripts/write_codedeploy_config.sh
+++ scripts/write_codedeploy_config.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-mkdir -p /var/codedeploy/tomcat-sample
+mkdir -p /var/codedeploy/thebodgeit
 
-cat <<EOF >/var/codedeploy/tomcat-sample/env.properties
+cat <<EOF >/var/codedeploy//thebodgeit/env.properties
 APPLICATION_NAME=$APPLICATION_NAME
 DEPLOYMENT_GROUP_NAME=$DEPLOYMENT_GROUP_NAME
 DEPLOYMENT_ID=$DEPLOYMENT_ID
